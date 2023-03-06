93eb77c620e178bd3cd14e088ef801ec32077559
marubillah57 <marufbillah5157@gmail.com>
2023-03-06 22:28:51 +0600

    Dragon new client

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_Xne6m5xOs2oNVCQbn1kTpOhp4WCq3Bk",
+  authDomain: "dragon-news-9cdf6.firebaseapp.com",
+  projectId: "dragon-news-9cdf6",
+  storageBucket: "dragon-news-9cdf6.appspot.com",
+  messagingSenderId: "44740871883",
+  appId: "1:44740871883:web:1b0f2ff27e36eb1ba4fd4e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
